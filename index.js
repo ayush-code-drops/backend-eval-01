2a8257e7cde8da07eda33c529bfd6b1774d8d06c
ayush-code-drops <ayushmangal121212@gmail.com>
2022-04-18 20:49:49 +0530

    checkPermission middlewares added

--- index.js
+++ index.js
@@ -2,21 +2,47 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//Logger Middleware
+
+const logger = (req, res, next) => {
+    const date=new Date().toTimeString()
+    console.log(`logged at ${date} for the Request path -> ${req.url}`)
+    next()
+}
+
+//using Logger middleware in the app
+app.use(logger)
+
+//Checking Permission Middleware 
+
+const checkPermission = (Route) => {
+    return (req, res, next) => {
+        console.log('In checkPermission middleware');
+        if (req.url == Route) {
+           console.log('Permission Granted! Route is same as requested');
+        }
+        else {
+            console.log('Permission Not Granted! Route is different from the requested path');
+        }
+    next()
+  }  
+}
+
 //Get Request for /books Route
 
 app.get('/books', (req, res, next) => {
     console.log('books route');
-    res.send('This is Books Page')
+  res.json({route:req.url})
 })
 //Get Request for /libraries Route
 
-app.get('/libraries', (req, res, next) => {
+app.get('/libraries',checkPermission('/libraries'), (req, res, next) => {
     console.log('Libraries Route');
     res.json({"route":req.url,"permission":true})
 })
 //Get Request for /authors Route
 
-app.get('/authors', (req, res, next) => {
+app.get('/authors', checkPermission('/authors'), (req, res, next) => {
     console.log('Authors route');
     res.json({"route":req.url,"permission":true})
 })
